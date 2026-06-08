ea5a1ceaa3e699d9fedefc7b3e298ebeed36ca8e
Suryanadhif <2313020199@mhs.univ.ac.id>
2026-06-08 20:32:39 +0700

    [CHORE] Integrasi awal frontend-backend secara lokal

--- project-mvc/app.py
+++ project-mvc/app.py
@@ -1,4 +1,8 @@
-from views.dashboard_component import render_dashboard
+from controllers.api_handler import get_users
+from views.dashboard_component import (
+    fetch_data_from_api,
+    render_dashboard
+)
 
 app_state = {
     "items": [],
@@ -9,16 +13,17 @@ def update_state(new_data):
     app_state["items"] = new_data
     app_state["is_loading"] = False
 
+
 if __name__ == "__main__":
 
+    # Loading State Minggu 10
     render_dashboard([], True)
 
-    mock_data = [
-        {"id": 101, "name": "Produk A"},
-        {"id": 102, "name": "Produk B"}
-    ]
+    # Integrasi API Minggu 11
+    data = fetch_data_from_api(get_users)
 
-    update_state(mock_data)
+    if data:
+        update_state(data)
 
     render_dashboard(
         app_state["items"],
--- project-mvc/views/dashboard_component.py
+++ project-mvc/views/dashboard_component.py
@@ -1,10 +1,26 @@
+def fetch_data_from_api(api_function):
+    print("[System] Mencoba menghubungkan ke API...")
+
+    try:
+        response = api_function()
+
+        if response["status"] == "success":
+            return response["data"]
+        else:
+            raise Exception("API Return Error")
+
+    except Exception as e:
+        print(f"[Error] Gagal Integrasi: {e}")
+        return None
+
+
 def render_dashboard(data_list, is_loading=False):
 
     if is_loading:
         print("Mohon Tunggu...")
         return
 
-    print("--- DASHBOARD APLIKASI ---")
+    print("\n--- DASHBOARD APLIKASI ---")
 
     if not data_list:
         print("[!] Data Kosong. Silakan sinkronisasi dengan Backend.")
